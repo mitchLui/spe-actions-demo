b45bc1ae73e7c987b52553df933bf4935f7a5234
Mitch Lui <mitch@mitchlui.dev>
2022-10-18 11:45:32 +0100

    spring boot

---
+++ src/test/java/com/mitchlui/actions/ActionsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.mitchlui.actions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+  @Test 
+  void testProcessAddRequest() {
+    assertEquals(5, Controller.processAddRequest(2, 3));
+  }
+
+  @Test
+  void testProcessSubtractRequest() {
+    assertEquals(5, Controller.processSubtractRequest(10, 5));
+  }
+
+}
