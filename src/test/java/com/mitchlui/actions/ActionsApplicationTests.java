4d1ff83865649e1f55b96e0ca2319131caeb23c3
Mitch Lui <mitch@mitchlui.dev>
2022-10-23 20:41:33 +0100

    initial commit

---
+++ src/test/java/com/mitchlui/actions/ActionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mitchlui.actions;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
